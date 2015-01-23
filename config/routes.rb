848f7051f6c092289c7f53e71f8dbf7cba5b40dd
Melissa Patterson <melissa.patterson.va@gmail.com>
2015-01-23 11:33:08 -0600

    add edit endpoint and form

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Metube::Application.routes.draw do
   get "/videos/:id", to: "videos#show"
   post '/videos' => 'videos#create'
   get '/videos/:id/edit', to: 'videos#edit'
-  put '/videos/:id', to: 'videos#update'
+  put '/videos/:id/edit', to: 'videos#update'
 
 
 end
