b9957646712fdd382f7717de156649eaad5bb539
Melissa Patterson <melissa.patterson.va@gmail.com>
2015-01-23 23:30:06 -0600

    add restful routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Metube::Application.routes.draw do
-  root to: "videos#index"
-  get "/videos", to: "videos#index"
-  get "/videos/new", to: "videos#new"
-  get "/videos/:id", to: "videos#show"
-  post '/videos' => 'videos#create'
-  get '/videos/:id/edit', to: 'videos#edit'
-  put '/videos/:id/edit', to: 'videos#update'
-  delete '/videos/:id', to: 'videos#destroy'
+  root "videos#index"
+  resources :videos
+  
+  # get "/videos", to: "videos#index"
+#   get "/videos/new", to: "videos#new"
+#   get "/videos/:id", to: "videos#show"
+#   post '/videos' => 'videos#create'
+#   get '/videos/:id/edit', to: 'videos#edit'
+#   put '/videos/:id/edit', to: 'videos#update'
+#   delete '/videos/:id', to: 'videos#destroy'
 
 end
